555b3c0a3c2478cc4125decedf84b62928001e1a
Wojtek Czerniak <wojciech.cz7@gmail.com>
2022-05-01 19:07:36 +0200

    Add first Controller and Routing

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,24 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController] // optional, useful
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            this._courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+
+            return new JsonResult(authorsFromRepo); // formats object as JSON
+        }
+    }
+}
